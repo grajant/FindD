993bd2a7ec20799b2f5a37931b6aee14764c6276
grajant <glego40_@hotmail.com>
2017-12-06 03:48:22 -0500

    Initial commit

---
+++ app/src/main/java/com/find_d/findd/Tags.java
@@ -0,0 +1,15 @@
+package com.find_d.findd;
+
+/**
+ * Created by Usuario on 16/10/2017.
+ */
+
+public class Tags {
+    public final static String TAG_NAME = "name";
+    public final static String TAG_EMAIL = "email";
+    public final static String TAG_URLIMG = "imgUrl";
+    public final static String LOGIN_OPTION = "optlog";
+    public final static String TAG_PREFERENCES = "MisPreferencias";
+    public final static String TAG_PASSWORD = "password";
+    public final static String TAG_KEY = "key";
+}
